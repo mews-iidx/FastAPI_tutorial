ce570f4d0dfc4aeec3a2e079e5249972202044f5
y.yamauchi <y.yamauchi@edge-consulting.jp>
2020-02-10 20:28:42 +0900

    upload files, path

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-from fastapi import FastAPI
+from typing import List
+
+from fastapi import FastAPI, File, UploadFile
+from starlette.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
 
-    @app.get("/items/{item_id}")
-    def read_item(item_id: int, q: str = None):
-        return {"item_id": item_id, "q": q}
+@app.post("/files/")
+async def create_files(files: List[bytes] = File(...)):
+    return {"file_sizes": [len(file) for file in files]}
+
+
+@app.post("/uploadfiles/")
+async def create_upload_files(files: List[UploadFile] = File(...)):
+    return {"filenames": [file.filename for file in files]}
+
+
+@app.get("/")
+async def main():
+    content = """
+<body>
+<form action="/files/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
