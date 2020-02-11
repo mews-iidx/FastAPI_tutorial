2a9c4a4e4fc9eaa3ba3f3a88fda0a47f79e44e1b
mews <mews@mews-base.com>
2020-02-11 18:41:22 +0900

    get binary data

--- main.py
+++ main.py
@@ -2,12 +2,29 @@ from typing import List
 
 from fastapi import FastAPI, File, UploadFile
 from starlette.responses import HTMLResponse
+from PIL import Image
+import io
+
+import cv2
+import numpy as np
 
 app = FastAPI()
 
 
+def byte2cv(img_binary):
+	img_binarystream = io.BytesIO(img_binary)
+	img_pil = Image.open(img_binarystream)
+	img_numpy = np.asarray(img_pil)
+	img_numpy_bgr = cv2.cvtColor(img_numpy, cv2.COLOR_RGBA2BGR)
+	return img_numpy_bgr
+
 @app.post("/files/")
 async def create_files(files: List[bytes] = File(...)):
+    for idx, file in enumerate(files):
+        cv_img = byte2cv(file)
+        #img = cv2.imdecode(nparr, cv2.CV_LOAD_IMAGE_COLOR) # cv2.IMREAD_COLOR in OpenCV 3.1
+        cv2.imwrite('img{}.jpg'.format(idx), cv_img)
+
     return {"file_sizes": [len(file) for file in files]}
 
 
