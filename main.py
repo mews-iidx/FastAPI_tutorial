698fd710880d4fb6721e05394f14052ad88219dc
mews <mews@mews-base.com>
2020-02-11 19:37:50 +0900

    complete

--- main.py
+++ main.py
@@ -1,41 +1,44 @@
 from typing import List
-
 from fastapi import FastAPI, File, UploadFile
 from starlette.responses import HTMLResponse
-from PIL import Image
-import io
-
-import cv2
-import numpy as np
-
+import os
+import request_predict
 app = FastAPI()
-
-
-def byte2cv(img_binary):
-	img_binarystream = io.BytesIO(img_binary)
-	img_pil = Image.open(img_binarystream)
-	img_numpy = np.asarray(img_pil)
-	img_numpy_bgr = cv2.cvtColor(img_numpy, cv2.COLOR_RGBA2BGR)
-	return img_numpy_bgr
 
 @app.post("/files/")
 async def create_files(files: List[bytes] = File(...)):
-    for idx, file in enumerate(files):
-        cv_img = byte2cv(file)
-        #img = cv2.imdecode(nparr, cv2.CV_LOAD_IMAGE_COLOR) # cv2.IMREAD_COLOR in OpenCV 3.1
-        cv2.imwrite('img{}.jpg'.format(idx), cv_img)
-
     return {"file_sizes": [len(file) for file in files]}
 
 
 @app.post("/uploadfiles/")
 async def create_upload_files(files: List[UploadFile] = File(...)):
     fnames = []
-    for idx, f in enumerate(files):
+    bins = []
+    for  f in files:
         fnames.append(f.filename)
-        cv_img = byte2cv(f.file.read())
-        cv2.imwrite(f.filename, cv_img)
-    return {'message' : 'file saved', 'files': fnames}
+        bins.append(f.file.read())
+    endpoint = os.getenv("ENDPOINT")
+    rets = request_predict.request_bins(bins, endpoint)
+
+    placeholder = {
+            'filename' : 'filename', 
+            'classes' : [],
+            'probs' : []
+        }
+    names = []
+    probs = []
+
+    return_dict = []
+    for img_file, ret in zip( fnames, rets):
+        for _, name, prob in ret:
+            names.append(name)
+            probs.append(prob)
+        placeholder['filename'] = img_file
+        placeholder['classes'] = names
+        placeholder['probs'] = probs
+        return_dict.append(placeholder)
+        
+    return {'return' : return_dict}
             
 
 
--- request_predict.py
+++ request_predict.py
@@ -5,6 +5,9 @@ import cv2
 import requests
 import sys
 import glob
+import io
+from PIL import Image
+import numpy as np
 
 payload_temp = {
     "data": {
@@ -15,6 +18,34 @@ payload_temp = {
 
 headers = {'content-type': 'application/json'}
 
+
+def byte2cv(img_binary):
+	img_binarystream = io.BytesIO(img_binary)
+	img_pil = Image.open(img_binarystream)
+	img_numpy = np.asarray(img_pil)
+	img_numpy_bgr = cv2.cvtColor(img_numpy, cv2.COLOR_RGBA2BGR)
+	return img_numpy_bgr
+
+
+def request_bins(binaries, endpoint):
+    imgs = []
+    for binary in binaries:
+        image = byte2cv(binary)
+        print(image.shape)
+        target_size = (224, 224)
+        img_resized = cv2.resize(image, target_size)
+        imgs.append(img_resized.tolist())
+
+    payload_temp["data"]['ndarray'] = imgs
+
+    resp = requests.post(
+        endpoint,
+        data=json.dumps(payload_temp),
+        headers=headers
+    )
+    result_dict = json.loads(resp.content.decode('utf-8'))
+    rets = result_dict['data']['ndarray']
+    return rets
 
 def request_predict(filenames, endpoint):
     imgs = []
