f690a3ef071a7246d2234ff5d57d9ce332152912
y.yamauchi <y.yamauchi@edge-consulting.jp>
2020-02-14 12:53:57 +0900

    add static show images

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from typing import List
 from fastapi import FastAPI, File, UploadFile
-from starlette.responses import HTMLResponse
+from starlette.responses import HTMLResponse, StreamingResponse
 import os
 import request_predict
 
-from starlette.templating import Jinja2Templates  # new
+from starlette.templating import Jinja2Templates 
 from starlette.requests import Request
+from starlette.staticfiles import StaticFiles
+
+import cv2
+import io
 
 app = FastAPI()
-
-@app.post("/files/")
-async def create_files(files: List[bytes] = File(...)):
-    return {"file_sizes": [len(file) for file in files]}
-
+templates = Jinja2Templates(directory="templates")
+app.mount('/uploadfiles/static', StaticFiles(directory='static'), name='static')
 
 @app.post("/uploadfiles/")
-async def create_upload_files(files: List[UploadFile] = File(...)):
+async def create_upload_files(request: Request, files: List[UploadFile] = File(...)):
     fnames = []
     bins = []
     for  f in files:
@@ -42,28 +43,11 @@ async def create_upload_files(files: List[UploadFile] = File(...)):
         placeholder['probs'] = probs
         return_dict.append(placeholder)
         
-    return {'return' : return_dict}
+    return templates.TemplateResponse('upload_files.html', {'request' : request, 'results' : return_dict  })
             
 
 
 @app.get("/")
-async def main():
-    content = """
-    			<body>
-    			<form action="/files/" enctype="multipart/form-data" method="post">
-    			<input name="files" type="file" multiple>
-    			<input type="submit">
-    			</form>
-    			<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
-    			<input name="files" type="file" multiple>
-    			<input type="submit">
-    			</form>
-    			</body>
-    """
-    return HTMLResponse(content=content)
+async def main(request: Request):
+    return templates.TemplateResponse('index.html', {'request' : request})
 
-## new テンプレート関連の設定 (jinja2)
-#templates = Jinja2Templates(directory="templates")
-#jinja_env = templates.env  # Jinja2.Environment : filterやglobalの設定用
-#
-#def index(request: Request):
